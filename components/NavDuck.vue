c8920f9d703d502fa1817c1c0843b1c6e5484989
poramin5202 <poramin5202@gmail.com>
2023-05-08 14:22:17 +0700

    test navduck

--- components/NavDuck.vue
+++ components/NavDuck.vue
@@ -1,94 +1,40 @@
 <template>
     <div>
-    <v-toolbar
+        <v-app-bar
+      color="#900603"
+      dense
+      flat
       dark
-      src="/vbanner.jpg"
-    > 
-
-      <v-app-bar-nav-icon>  
-        <v-btn icon @click="gohome">
-        <v-icon>mdi-home</v-icon> 
-      </v-btn>
-    </v-app-bar-nav-icon>
-
-      <v-toolbar-title class="text-center pl-5">ร้านก๋วยเตี๋ยวเป็ดตุ๋นลุงเล็ก (หัวไผ่)</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-     <!-- <v-btn icon @click="logout">
-            <v-icon>mdi-export</v-icon>
-        </v-btn>
-    -->
-    </v-toolbar>
-    <v-dialog
-      v-model="dialog"
-      persistent
-      max-width="290"
+      height="60"
     >
-      <v-card shaped >
-        <v-card-title class="text-h5">
-         <h6>คุณต้องการออกจากระบบหรือไม่</h6> 
-        </v-card-title>
+    <v-toolbar-title>
+       <h4> ก๋วยเตี๋ยวเป็ดตุ๋น ลุงเล็ก (หัวไผ่)</h4> 
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-img src="/LogoDuck.png" height="50" contain class="ml-2 rotating-image resized-image"
+    style="width: 50px; "></v-img>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn
-            color="red"
-            text
-            @click="dialog = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            color="green"
-            text
-            @click="out"
-          >
-            Ok
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+        </v-app-bar>
   </div>
 </template>
 
-<script>
-  export default { 
-    async mounted() {
-      if(this.$store.getters.getUser.role != "A"){
-        this.$store.dispatch('setUser', this.form)
-       // this.$router.push("/login")
-      }
-    },
-    data(){
-      return{
-        dialog:false,
-        form:{
-          id:null,
-          pwd:null,
-          name:null,
-          dialogcheck:false
-        }
-      }
-    },
-  methods:{
-    out(){
-      this.$store.dispatch('setUser', this.form)
-        //this.$router.push("/login")
-    },
-      logout(){
-        this.dialog=true
-      },
-      gohome(){
-     this.$router.push("/home")
-      }
-  },
-      computed: {
-          getUser(){
-             return this.$store.getters.getUser;
-          },
-          
-      },
-      
-      
-  };
-  </script>
+<style lang="scss" scoped>
+.rotating-image {
+    animation: rotation 8s infinite linear;
+    transform-origin: center;
+    transform-style: preserve-3d;
+    animation: rotation 8s infinite linear; //ความเร็วในการหมุนภาพ
+  }
+  @keyframes rotation {
+    from {
+      transform: rotateY(0deg);
+    }
+    to {
+      transform: rotateY(360deg);
+    }
+  }
+  .resized-image {
+    width: 25px;
+    height: 25px;
+  }
+</style>
